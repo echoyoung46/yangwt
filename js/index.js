aa904754616f2a26912c3575670f18e4574a9a81
echoyoung46 <vickyyoung0406@yeah.net>
2015-08-15 23:52:24 +0800

    add all files

--- js/index.js
+++ js/index.js
@@ -1,28 +1,133 @@
 $(function() {
-	drawGraph();
 	initAction();
-	$("#nav-toggle").click(function(event) {
-		$(".pull").slideToggle();
-	});
-
+	createList();
+	initFlexslider();
+	drawGraph();
 })
 
-/**
- * canvas绘制图形
- * @return {[type]} [description]
- */
-function drawGraph() {
-
-}
 
 /**
  * 绑定DOM事件
  * @return {[type]} [description]
  */
 function initAction() {
+	$("#nav-toggle").click(function(event) {
+		$(".pull").slideToggle();
+	});
 
+	document.querySelector("#nav-toggle").addEventListener("click", function() {
+		this.classList.toggle("active");
+	});
 }
 
-document.querySelector("#nav-toggle").addEventListener("click", function() {
-	this.classList.toggle("active");
-});
+/**
+ * 创建作品展示列表
+ * @return {[type]} [description]
+ */
+function createList() {
+	var imgArr = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg"];
+	var html = '';
+	var distance = 130;
+	var left = 0;
+	$.each(imgArr, function(i, val) {
+		left = i * distance + "px";
+		html += '<li style="left: ' + left + ';"><img src="image/project/' + val + '"></li>';
+	});
+	$("#experience_thumb ul").html(html);
+	var listObj = {
+		"list" : imgArr,
+		"container": "#experience_url",
+		"thumb_container": "#experience_thumb",
+		"left_arrow": ".experience_arrow_left",
+		"right_arrow": ".experience_arrow_right",
+		"position": 0,
+		"count": 5,
+		"distance": distance
+	};
+
+	var viewList = new Viewlist( listObj );
+	viewList.init();
+	$(".experience_arrow_left").on('click', function() { viewList.moveFocus(-1); });
+	$(".experience_arrow_right").on('click', function() { viewList.moveFocus(1); });
+}
+
+/**
+ * 初始化滑动块
+ */
+function initFlexslider() {
+	$('#teamSlider').flexslider({
+		animation: "slide",
+		directionNav: false,
+		controlNav: true,
+		touch: true,
+		pauseOnHover: true,
+		start: function() {
+			$.waypoints('refresh');
+		}
+	});
+}
+
+function drawGraph() {
+	var graphObj = [
+		{"dom":"myHTML", "num":"70", "name":"HTML5"},
+		{"dom":"myCSS", "num":"70", "name":"CSS3"},
+		{"dom":"myJS", "num":"80", "name":"JavaScript"},
+		{"dom":"myJQ", "num":"70", "name":"jQuery"},
+		{"dom":"myPHP", "num":"50", "name":"PHP"}
+	];
+	$.each(graphObj, function(i, val) {
+		drawCircle(val.dom, val.num, val.name);
+	});
+}
+/**
+ * canvas绘制原型
+ * @return {[type]} [description]
+ */
+function drawCircle(container, percentage, text) {
+	var circle = percentage / 100;
+	var canvas = document.getElementById(container);
+	var ctx = canvas.getContext('2d');
+	ctx.clearRect(0, 0, 200, 200);
+
+	//画圆
+	ctx.beginPath();
+	ctx.moveTo(100, 100);
+	ctx.arc(100, 100, 100, 0, Math.PI * 2, false);
+	ctx.closePath();
+	ctx.fillStyle = "#ddd";
+	ctx.fill();
+
+	//画进度
+	ctx.beginPath();
+	ctx.moveTo(100, 100);
+	ctx.arc(100, 100, 100, 0, Math.PI*2*circle, false);
+	ctx.closePath();
+	ctx.fillStyle = "#e74c3c";
+	ctx.fill();
+
+	//画内部空白
+	ctx.beginPath();
+	ctx.moveTo(100, 100);
+	ctx.arc(100, 100, 95, 0, Math.PI*2, true);
+	ctx.closePath();
+	ctx.fillStyle = "#008080";
+	ctx.fill();
+
+	// 画一条线  
+    ctx.beginPath();  
+    ctx.arc(100, 100, 90, 0, Math.PI * 2, true);  
+    ctx.closePath();  
+    // 与画实心圆的区别,fill是填充,stroke是画线  
+    ctx.strokeStyle = '#ddd';  
+    ctx.stroke();  
+      
+    //在中间写字 
+    var text = text + '    ' + percentage + "%"; 
+    ctx.font = "bold 15pt Arial";  
+    ctx.fillStyle = '#e74c3c';  
+    ctx.textAlign = 'center';  
+    ctx.textBaseline = 'middle';  
+    ctx.moveTo(100, 100);  
+    ctx.fillText(text, 100, 100);  
+
+}
--- js/viewlist.js
+++ js/viewlist.js
@@ -1,3 +1,54 @@
 function Viewlist(_param) {
-	
+	this.listArr = _param.list;	//图片列表
+	this.container = _param.container;	//大图容器
+	this.thumb_container = _param.thumb_container;	//缩略图容器
+	this.left_arrow = _param.left_arrow,	//左箭头容器
+	this.right_arrow = _param.right_arrow,	//右箭头容器
+	this.pos = _param.position;	//焦点位置,从0开始
+	this.showCount = _param.count;	//显示出来的条目数量
+	this.distance = _param.distance;	//左移距离
+
+	var maxLeft = this.distance * (this.listArr.length - 2),
+		keyFlag = true;	//click事件是否响应
+
+	//初始化
+	this.init = function() {
+		$(this.thumb_container).find("li").eq(this.pos).addClass('select');
+
+		var imgUrl = $(this.thumb_container).find("img").eq(this.pos).attr("src");
+		$(this.container).attr("src", imgUrl);
+
+	}
+	//焦点移动
+	this.moveFocus = function( _num ) {
+		if( keyFlag == false ) {
+			console.log("false");
+			return;
+		}
+		keyFlag == false;
+		var distance = this.distance;
+		if( this.pos + _num < 0 ){
+			this.pos = 0;
+		}else if( this.pos + _num >= this.showCount ) {
+			this.pos += _num;
+			$(this.thumb_container).find("li").each(function(i, ele) {
+				var currLeft = parseInt($(ele).css("left"));
+				var moveLeft = currLeft - distance;
+				if( moveLeft < -130 ){
+					moveLeft = maxLeft;
+					console.log(moveLeft);
+				}
+				$(ele).css("left", moveLeft + "px");
+			});
+		}else{
+			this.pos += _num;
+		}
+		console.log(this.pos);
+		$(this.thumb_container).find("li").eq(this.pos).addClass('select').siblings().removeClass('select');
+		var imgUrl = $(this.thumb_container).find("img").eq(this.pos).attr("src");
+		$(this.container).attr("src", imgUrl);
+		setTimeout(function(){
+			keyFlag = true;
+		},2000);
+	}
 }
